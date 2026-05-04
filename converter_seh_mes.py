acbb735a50c09d5733429ddecc8063ee87079d0e
Adryan Victor Oliveira Gomes <adryan.gomes@arapiraca.ufal.br>
2026-05-04 07:10:43 -0300

    Delete converter_seh_mes.py

--- converter_seh_mes.py
+++
@@ -1,8 +0,0 @@
-segundos_totais = int(input())
-horas = segundos_totais // 3600
-segundos_restantes = segundos_totais % 3600
-
-minutos = segundos_restantes // 60
-segundos = segundos_restantes % 60
-
-print(f"{horas} h {minutos} m {segundos} s")
