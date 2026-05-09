b40f4d3e64da8a09b620583c408bf9aa2986fbfb
Adryan Victor Oliveira Gomes <adryan.gomes@arapiraca.ufal.br>
2026-05-09 11:03:47 -0300

    Add files via upload

---
+++ atividades_de_apc/estrutura_de_condicao_e_operação_logica/easy_jet.py
@@ -0,0 +1,13 @@
+largura = float(input())
+comprimento = float(input())
+altura = float(input())
+
+if largura > 45:
+    print("PROIBIDA")
+elif comprimento > 56:
+    print("PROIBIDA")
+elif altura > 25:
+    print("PROIBIDA")
+else:
+    print("PERMITIDA")
+
